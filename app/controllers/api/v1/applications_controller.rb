27c16d363cf2919ae51760a8a12d8bc805a9a68c
Mazen Sayed <mazen.sayed@robustastudio.com>
2022-08-05 18:55:38 +0200

    added redis caching layer for apps/chats/messages

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -4,13 +4,22 @@ module Api
       protect_from_forgery with: :null_session
 
       def index
+        cached_apps = Rails.cache.read("applications")
+
+        if !cached_apps.blank?
+          render json: {status: 'SUCCESS', message: 'Loaded Apps', data: cached_apps}, status: :ok
+          return
+        end
+
         applications = Application.order('created_at DESC');
+        Rails.cache.write("applications", applications, expire_in: 5.minutes)
         render json: {status: 'SUCCESS', message: 'Loaded Apps', data: applications}, status: :ok
       end
 
       def create
         token = create_token;
         application = Application.create({name: params[:name],token: token})
+        Rails.cache.delete("applications")
         if application.save
           render json: {status: 'SUCCESS', message: 'Create app', data: application.as_json(only: [:token, :name, :created_at])}, status: :ok
         else
--- app/controllers/api/v1/chats_controller.rb
+++ app/controllers/api/v1/chats_controller.rb
@@ -4,11 +4,18 @@ module Api
       protect_from_forgery with: :null_session
 
       def show
+        cached_chats = Rails.cache.read("get_chats-"+params[:application_token])
+
+        if !cached_chats.blank?
+          render json: {status: 'SUCCESS', message: 'Loaded Chats', data: cached_chats.as_json(only: [:chat_num, :messages_count, :created_at])}, status: :ok
+          return
+        end
         application = Application.where(token: params[:application_token])
 
         if application
           chats = application[0].chats
-          render json: {status: 'SUCCESS', message: 'Loaded Apps', data: chats.as_json(only: [:chat_num, :messages_count, :created_at])}, status: :ok
+          Rails.cache.write("get_chats-"+params[:application_token], chats, expire_in: 5.minutes)
+          render json: {status: 'SUCCESS', message: 'Loaded Chats', data: chats.as_json(only: [:chat_num, :messages_count, :created_at])}, status: :ok
         else
           render json: {status: 'ERROR'}, status: :not_found
         end
@@ -26,6 +33,9 @@ module Api
           if chat.save
             # Call async method to update chats_count of this application as the chat is saved
             UpdateChatsCountJob.perform_later(params[:application_token])
+
+            Rails.cache.delete("get_chats-"+params[:application_token])
+
             render json: {status: 'SUCCESS', message: 'Create app', data: chat.as_json(only: [:chat_num, :created_at])}, status: :ok
           else
             render json: {status: 'ERROR', message: 'Application not created', data: chat.errors }, status: :unprocessable_entity
